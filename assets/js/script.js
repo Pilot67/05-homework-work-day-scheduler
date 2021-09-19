9132c9e24733be09bb580efcb6c0816b5a5ce785
Stuart Simmons <stuart@Stuart.local>
2021-09-20 09:45:47 +1000

    added 'save' and tidied up js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,30 @@ var timeStamp = $('#currentTime');
 var timeNow = moment().format("Do MMM YYYY hh:mm");
 var currentHour = parseInt(moment().format("H"));
 var setHr;
-var dayArr = [];
 
 //set up the array for storing the calendar
+var dayArr = []; 
 dayArr.length = 9;
+getPlanner();
 
-//for testing only - remove before deploying
-dayArr[0] = "This is Hour 9 \n Some more text";
-dayArr[8] = "This is Hour 17";
-dayArr[3] = "This is hour 12";
-
-
-for (index = 9; index <= 17; index++){
-    var timeLine = $('<div>').attr({class: "row time-block", id:"hourRow", "data-hour": index});
-    var timeEl = $('<div>').attr({class: "col-2 col-lg-1 pt-3 hour"}).text(moment(index,"H").format("h:00a",index));
-    var dayEl = $('<div>').attr({class: "col-8 col-lg-10 future", id:"hourDisplay"});
-    var inputEl = $('<textarea>').attr({class: "form-control border-0 mt-1 p-0 future", name:"hourPlan", id:"hourPlan", rows:"3", cols:"30"});
-    var saveEl = $('<div>').attr({class: "col-2 col-lg-1 btn saveBtn far fa-save exSave"});
-    inputEl.text(dayArr[index-9])
-    dayEl.append(inputEl);
-    timeLine.append(timeEl);
-    timeLine.append(dayEl);
-    timeLine.append(saveEl);
-    timeBlockEl.append(timeLine);
+function printScreen(){
+    for (index = 9; index <= 17; index++){
+        var timeLine = $('<div>').attr({class: "row time-block", id:"hourRow", "data-hour": index});
+        var timeEl = $('<div>').attr({class: "col-2 col-lg-1 pt-3 hour"}).text(moment(index,"H").format("h:00a",index));
+        var dayEl = $('<div>').attr({class: "col-8 col-lg-10 future", id:"hourDisplay"});
+        var inputEl = $('<textarea>').attr({class: "form-control border-0 mt-1 p-0 future", name:"hourPlan", id:"hourPlan", rows:"3", cols:"30"}).val(dayArr[index-9]);
+        var saveEl = $('<div>').attr({class: "col-2 col-lg-1 btn saveBtn far fa-save exSave", "data-btn":index-9});
+        dayEl.append(inputEl);
+        timeLine.append(timeEl);
+        timeLine.append(dayEl);
+        timeLine.append(saveEl);
+        timeBlockEl.append(timeLine);
+    }
 }
-
 function printTime (){
     timeStamp.text(moment().format("dddd Do MMM  h:mm:ssa")); //prints the current time to the header
     currentHour = parseInt(moment().format("H")); //sets the current hour as integer
+    printTense(); //run the printTense function to see if the hour has changed
 };
 
 function printTense (){
@@ -53,19 +49,26 @@ function printTense (){
         }
     }
 }
-
-function savePlanner (){
-    localStorage.setItem("dayPlanner", JSON.stringify(dayArr));
-}
+//click listener
+timeBlockEl.on('click', '.saveBtn',function(event){
+    event.preventDefault();
+    var i = parseInt($(event.target).data("btn"));
+    var textEl = $(event.target).prev().children().val().trim();
+    dayArr[i] = textEl;
+    localStorage.setItem("dayPlanner", JSON.stringify(dayArr)); //save the array to local storage
+    $(event.target).prev().children().val(dayArr[i]) //repaint the textarea
+})
 
 function getPlanner(){
-    JSON.parse(localStorage.getItem("dayPlanner"),dayArr);
+    dayArr = JSON.parse(localStorage.getItem("dayPlanner"));
     console.log(dayArr)
 }
 
-setInterval (printTime,1000); //set a timer for 60 seconds
+function init(){
+    setInterval (printTime,1000); //set a timer for 60 seconds
+    getPlanner(); //get the local storage array
+    printScreen(); //print the screen initial
+    printTime(); 
+}
 
-printTime();
-//savePlanner();
-getPlanner();
-printTense();
+init();
